4eef73197249544b5d32338bca23d4c283ecdcd8
soft-dev3 <mdsaddamkhan320@gamil.com>
2024-03-20 22:58:40 +0600

    done

---
+++ array-split.js
@@ -0,0 +1,8 @@
+const num=[1,2,3,4,5,6,7,8,9];
+const part = num.slice(3,6);
+console.log(part);
+const part1=num.splice(2,4, 209);
+console.log(part1);
+const together=num.join("new");
+
+console.log(together);
